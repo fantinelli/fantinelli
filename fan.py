3d3c0131fef5df0f74456a0b50329601fa68016c
fantinelli <fernandaapbercelino@gmail.com>
2023-07-25 09:04:06 -0300

    UPload arquivo fan

---
+++ fan.py
@@ -0,0 +1,112 @@
+import pyautogui as py
+import time
+import os
+
+def enter(): 
+    py.press('enter')
+    time.sleep(1)
+    
+def pressione(x):
+    py.press(x)
+    time.sleep(1)
+    
+def escreve(x):
+    py.write(x)
+    
+    
+
+    
+    
+
+
+
+
+
+pressione('win')
+time.sleep(1)
+escreve('excel')
+enter()
+time.sleep(1)
+enter()
+escreve("ALEXANDRE BAGINI")
+enter()
+escreve("ALEXANDRE LEMES")
+enter()
+escreve("ANA CAROLINE MORAES MALHEIRO")
+enter()
+escreve("ANA JÚLIA FELISBERTO BUENO")
+enter()
+escreve("ANDRÉ FELIPE ROSA DA SILVA")
+enter()
+escreve("ARTHUR MATHEUS DE MOURA")
+enter()
+escreve("CESAR AUGUSTO CAVALLARO")
+enter()
+escreve("DIEGO DA ROZ OTAVIANO")
+enter()
+escreve("ENZO BIASON MANHA")
+enter()
+escreve("GABRIEL SOARES NECO")
+enter()
+escreve("GIULIA DA CUNHA ROCHA")
+enter()
+escreve("GUILHERME AUGUSTO DOS SANTOS SILVA")
+enter()
+escreve("GUILHERME SOUZA LIMA")
+enter()
+escreve("ISABELLE DANIEL DOS ANJOS")
+enter()
+escreve("JOÃO PEDRO PORTOCARRERO")
+enter()
+escreve("JÚLIA MONTEIRO DALLA VECCHIA")
+enter()
+escreve("JULIANO SILVESTRE DIOGO")
+enter()
+escreve("KAUÊ LIMA BARBOSA")
+enter()
+escreve("LEONARDO RODRIGO CAETANO")
+enter()
+escreve("LETÍCIA RODRIGUES DE PAIVA")
+enter()
+escreve("MARIA EDUARDA DOS SANTOS")
+enter()
+escreve("MIRELLA TURQUETI CORREA GONÇALVES")
+enter()
+escreve("MURILO AUGUSTO CARNEIRO")
+enter()
+escreve("NATÁLIA PAZ DA SILVA")
+enter()
+escreve("NATALIA VAZ CERQUEIRA")
+enter()
+escreve("NATHAN HENRIQUE DA CUNHA SOUZA")
+enter()
+escreve("PABLO DA SILVA FERREIRA DE LIMA")
+enter()
+escreve("RAFAELA FATIMA DE SOUZA ALVES")
+enter()
+escreve("RUAN HENRIQUE PALMEIRA")
+enter()
+escreve("THAINÁ CRISTINA DE SOUZA")
+enter()
+escreve("VINICIUS DE OLIVEIRA MOURA")
+enter()
+escreve("VINICIUS FANTINELLI")
+enter()
+escreve("VÍTOR VINÍCIUS GONÇALVES")
+enter()
+escreve("YURI AZEVEDO ANTUNES DE SOUZA")
+
+os.system("pause")
+int(input("Digite a quantidade de pessoas presentes:"))
+str(input("Digite todos nomes presentes:"))
+
+pressione('win')
+time.sleep(1)
+escreve('excel')
+enter()
+time.sleep(1)
+enter()
+
+
+os.sytem("pause")
+
